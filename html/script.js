fabbcde1c10d9ae64781721a7d9737712f7f8ff1
tinchis <marcuad05@gmail.com>
2025-12-10 16:43:10 -0300

    a

--- html/script.js
+++ html/script.js
@@ -90,7 +90,7 @@ const openMenu = (data = null) => {
 
 const getButtonRender = (header, message = null, id, isMenuHeader, isDisabled, icon) => {
     return `
-        <div class="button w-full px-6 py-4 bg-zinc-900/30 backdrop-blur-sm border-b-[0.50px] border-zinc-800 flex justify-start items-center gap-4 transition-all duration-[250ms] ${isDisabled ? "opacity-20" : ""}" id="${id}">
+        <div class="button w-full px-6 py-3 bg-black/30 backdrop-blur-md border-b-[0.50px] border-zinc-800/40 flex justify-start items-center gap-4 transition-all duration-[250ms] ${isDisabled ? "opacity-20" : "hover:bg-black/40"}" id="${id}">
             <div class="icon p-2 bg-zinc-800/50 rounded-md outline outline-[0.50px] outline-offset-[-0.50px] outline-neutral-700 flex justify-start items-center gap-2.5 overflow-hidden">
                 <div class="w-5 h-5 relative overflow-hidden flex items-center justify-center">
                     <img src="nui://${icon}" width="20" class="block" onerror="this.onerror=null; this.remove();">
@@ -99,7 +99,7 @@ const getButtonRender = (header, message = null, id, isMenuHeader, isDisabled, i
             </div>
             <div class="column flex flex-col gap-0.5 flex-1">
                 <div class="header justify-start text-white text-sm font-medium font-['IBM_Plex_Mono'] uppercase">${header}</div>
-                ${message ? `<div class="text justify-start text-neutral-400 text-xs font-normal font-['IBM_Plex_Mono']">${message}</div>` : ""}
+                ${message ? `<div class="text justify-start text-neutral-500 text-xs font-normal font-['IBM_Plex_Mono']">${message}</div>` : ""}
             </div>
         </div>
     `;
